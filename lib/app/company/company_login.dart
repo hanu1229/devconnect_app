533858d29ad1c04b05e6a719d5b6e7ec2fe62171
magnoria <sidaumv@gmail.com>
2025-04-25 21:17:18 +0900

    2025-04-25 한상범 : flutter 로그인페이지 초안 완료

--- lib/app/company/company_login.dart
+++ lib/app/company/company_login.dart
@@ -19,8 +19,8 @@ class _CompanyLogin extends State<Companylogin>{
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Container(
-        padding: EdgeInsets.all(50),
-        margin: EdgeInsets.fromLTRB(60, 120, 60, 120),  // left, top , light, bottom
+        padding: EdgeInsets.fromLTRB(30, 50, 30, 120),
+        margin: EdgeInsets.fromLTRB(30, 150, 30, 230),  // left, top , light, bottom
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
@@ -28,32 +28,75 @@ class _CompanyLogin extends State<Companylogin>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [ // 하위 요소들 위젯
+            SizedBox(
+             width : double.infinity, // 넓이 화면 크기에 따라 자동 조절
+            child:
             TextField(
               controller: idController,
               decoration: InputDecoration(
                 labelText: "id",
                 border: OutlineInputBorder(),
+
               ),
-            ),
+            ),),
+
+
             SizedBox(height: 20,),
-            TextField(controller: pwdController,
+
+
+            SizedBox(
+              width: double.infinity,
+              child: TextField(
+              controller: pwdController,
               obscureText: true, // 입력값 감추기
               decoration: InputDecoration(
                 labelText: "비밀번호",
                 border: OutlineInputBorder(),
               ),
-            ),
+            ),),
+
             SizedBox(height: 20,),
-            ElevatedButton(onPressed: ()=>{}, child: Text("로그인"), style: ButtonStyle(backgroundColor: WidgetStateProperty.resolveWith<Color>(
+
+            SizedBox(
+              width: double.infinity,
+              child:
+            ElevatedButton(onPressed: ()=>{}, style: ButtonStyle(backgroundColor: WidgetStateProperty.resolveWith<Color>(
                 (Set<WidgetState> states) {
                   if(states.contains(WidgetState.pressed)){
                   return Colors.blue.shade700;
                 }
-                return Colors.blue; // 기본 배경색은 파란색
+                return Colors.blue; // 기본 배경색은 파란
                 }
-            ),),),
+            ),
+              shape: WidgetStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),  // 둥글기 크기 조절
+                )
+              ) ,
+
+            ), child: Text("로그인", style: TextStyle(color: Colors.black),),),),
+
             SizedBox(height: 20,),
-            TextButton(onPressed: ()=>{}, child: Text("회원가입")),
+
+            SizedBox(
+              width: double.infinity,
+              child:
+              ElevatedButton(onPressed: ()=>{}, style: ButtonStyle(backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                      (Set<WidgetState> states) {
+                    if(states.contains(WidgetState.pressed)){
+                      return Colors.blue.shade700;
+                    }
+                    return Colors.blue; // 기본 배경색은 파란
+                  }
+              ),
+                shape: WidgetStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),  // 둥글기 크기 조절
+                    )
+                ) ,
+
+              ), child: Text("회원가입", style: TextStyle(color: Colors.black),),),),
+
           ],
         ),
       ),
