c33fda500da7d712fef5f38d505c9a0bb92d53e6
hanu1229 <han494127@gmail.com>
2025-04-27 02:16:17 +0900

    4/26 한웅재 : 무한 스크롤 구현 완료(꾸미기 작업 진행 중)

--- lib/app/layout/home.dart
+++ lib/app/layout/home.dart
@@ -1,6 +1,7 @@
 // home.dart : 메인 페이지
 
-import "package:devconnect_app/style/app_colors.dart";
+import "package:devconnect_app/style/server_path.dart";
+import "package:dio/dio.dart";
 import "package:flutter/material.dart";
 
 class Home extends StatefulWidget {
@@ -11,62 +12,83 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final ScrollController _scrollController = ScrollController();
-  List<int> items = List.generate(20, (index) => index);
+
+  Dio dio = Dio();
+  int page = 0;
+  int size = 5;
+  // 빈 데이터가 오는지 확인하는 변수
+  bool hasNext = true;
+  // 로딩 확인 변수
   bool isLoading = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_scrollListener);
-  }
+  final ScrollController _scrollController = ScrollController();
 
-  void _scrollListener() {
-    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 100 && !isLoading) {
-      _loadMoreData();
+  List<dynamic> list = [];
+
+  Future<void> findData() async {
+    // 중복 호출 방지
+    if(isLoading || !hasNext) { return; }
+    setState(() { isLoading = true; });
+    try {
+      // 테스트를 위한 딜레이
+      await Future.delayed(Duration(seconds: 2));
+      final response = await dio.get("$serverPath/api/project/paging?page=$page&size=$size");
+      final data = response.data;
+      print(data);
+      if(data.length < size) { hasNext = false; }
+      setState(() {
+        list.addAll(data);
+        // 페이지 증가
+        page += 1;
+      });
+    } catch(e) {
+      print(e);
+    } finally {
+      setState(() { isLoading = false; });
     }
   }
 
-  Widget _buildLoader() {
-    return isLoading
-        ? Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Center(child: CircularProgressIndicator()),
-    ) : SizedBox.shrink();
-  }
-
-  void _loadMoreData() async {
-    setState(() => isLoading = true);
-
-    await Future.delayed(Duration(seconds: 2)); // API 대기 시뮬레이션
-
-    setState(() {
-      items.addAll(List.generate(10, (index) => items.length + index));
-      isLoading = false;
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
+ @override
+  void initState() {
+    super.initState();
+    findData();
+   // 스크롤 이벤트 등록
+   _scrollController.addListener(() {
+     if(_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 30) {
+       // 스크롤 끝에 도달하면 추가 데이터 불러오기
+       if(hasNext && !isLoading) { findData(); }
+     }
+   });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        scrollDirection : Axis.vertical,
-        controller: _scrollController,
-        itemCount: items.length + 1, // 로딩 인디케이터 때문에 +1
-        itemBuilder: (context, index) {
-          if (index == items.length) {
-            return _buildLoader();
-          }
-          return ListTile(title: Text('아이템 ${items[index]}'));
-        },
+      body: Column(
+        children : [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount : list.length + (isLoading ? 1 : 0),
+              itemBuilder : (BuildContext context, int index) {
+                if(index < list.length) {
+                  return ListTile(
+                    title : Text("${list[index]}"),
+                  );
+                } else {
+                  return Padding(
+                    padding : EdgeInsets.all(16),
+                    child : Center(
+                      child : CircularProgressIndicator(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          SizedBox(height : MediaQuery.of(context).size.height * 0.13),
+        ],
       ),
     );
   }
-
 }
