fc80f5bd7bd993f65776c725bdb52de40e49f011
PHM-dung2 <pp4992@naver.com>
2025-04-28 14:31:09 +0900

    4/28 박희만 : app_color 저장

--- lib/app/developer/profile.dart
+++ lib/app/developer/profile.dart
@@ -1,6 +1,8 @@
 
+import 'package:devconnect_app/style/app_colors.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 String path = "http://localhost:8080/api/developer";
 
@@ -12,13 +14,36 @@ class Profile extends StatefulWidget{
 }
 
 class _ProfileState extends State< Profile >{
+  
+  Dio dio = Dio();
+  
+  // 상태변수
+  int mno = 0;
+  String did = "";
+  String dname = "";
+  // 수정 상태 확인
+  bool isUpdate = false;
 
   @override
   void initState() {
 
   } // f end
+  
+  // 로그인 상태 확인
+  bool? isLogIn;
 
-  Dio dio = Dio();
+  void loginCheck() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    if( token != null && token.isNotEmpty ){
+      setState(() {
+        isLogIn = true; print("로그인 중");
+        onInfo( token );
+      });
+    }else{
+      // Navigator.pushReplacement( context, MaterialPageRoute(builder: ( context ) =>  ) )
+    }
+  } // f end
 
   Map<String, Object> dList = {};
 
@@ -29,7 +54,7 @@ class _ProfileState extends State< Profile >{
       final data = response.data;
       if( data != '' ){
         setState(() {
-
+          dList = data;
         });
       }
     }catch( e ){ print( e ); }
@@ -64,8 +89,9 @@ class _ProfileState extends State< Profile >{
 
                     SizedBox( height: 20 ,),
 
+                    // 첫번째 Card
                     SizedBox(
-                      height: 300,
+                      height: 500,
                       width: double.infinity,
                       child: Card(
                         shape: RoundedRectangleBorder(
@@ -73,14 +99,29 @@ class _ProfileState extends State< Profile >{
                           BorderRadius.circular(12),
                           side: BorderSide(
                             width: 1,
-                            color: Color(0xffccdbe3),
+                            color: AppColors.cardBorderColor,
                           ),
                         ),
                         elevation: 0,
                         color: Colors.white,
                         child: Padding(
                           padding: EdgeInsets.all( 20 ),
-                          child: Column(
+                          child:
+                          !isUpdate
+                            ? // 수정버튼 클릭 전
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("테스트"),
+                              OutlinedButton(
+                                onPressed: () => { setState(() => { isUpdate = true }) },
+                                child: Text("수정"),
+                              )
+                            ]
+                          )
+                            : // 수정버튼 클릭 후
+                          Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -91,20 +132,47 @@ class _ProfileState extends State< Profile >{
                               TextField(
                                 decoration: InputDecoration(
                                   filled: true,
-                                  fillColor: Color(0xfffbfbfb),
+                                  fillColor: AppColors.textFieldBGColor,
                                   enabledBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular( 12 ),
                                     borderSide: BorderSide(
                                       width: 1.5,
-                                      color: Color(0xffd5dae1),
+                                      color: AppColors.textFieldColor,
                                     ),
                                   ),
                                   focusedBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular( 12 ),
                                     borderSide: BorderSide(
-                                      width: 2.5,
-                                      color: Color(0xffa5adba),
-                                    )
+                                      width: 3,
+                                      color: AppColors.focusColor,
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              SizedBox( height: 12,),
+
+                              Text("이름"),
+
+                              SizedBox( height: 12,),
+
+                              TextField(
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: AppColors.textFieldBGColor,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular( 12 ),
+                                    borderSide: BorderSide(
+                                      width: 1.5,
+                                      color: AppColors.textFieldColor,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular( 12 ),
+                                      borderSide: BorderSide(
+                                        width: 3,
+                                        color: AppColors.focusColor,
+                                      )
                                   ),
                                 ),
                               ),
@@ -123,18 +191,38 @@ class _ProfileState extends State< Profile >{
                                     borderRadius: BorderRadius.circular( 12 ),
                                     borderSide: BorderSide(
                                       width: 1.5,
-                                      color: Color(0xffd5dae1),
+                                      color: AppColors.textFieldColor,
                                     ),
                                   ),
                                   focusedBorder: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular( 12 ),
                                       borderSide: BorderSide(
-                                        width: 2.5,
-                                        color: Color(0xffa5adba),
+                                        width: 3,
+                                        color: AppColors.focusColor,
                                       )
                                   ),
                                 ),
                               ),
+
+                              SizedBox( height: 15,),
+
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    onPressed: () => { setState(() => { isUpdate = false }) },
+                                    child: Text("이전"),
+                                  ),
+
+                                  SizedBox( width: 10,),
+
+                                  TextButton(
+                                    onPressed: () => { },
+                                    child: Text("저장")
+                                  ),
+                                ],
+                              )
 
                             ],
                           ),
--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -14,4 +14,13 @@ class AppColors {
   static const Color textColor = Color(0xFF000000);
   // #FFFFFF
   static const Color bgColor = Color(0xFFFFFFFF);
+  // #a5adba
+  static const Color focusColor = Color(0xff46B8FF);
+  // #d5dae1
+  static const Color textFieldColor = Color(0xffd5dae1);
+  // #fbfbfb
+  static const Color textFieldBGColor = Color(0xfffbfbfb);
+  // #ccdbe3
+  static const Color cardBorderColor = Color(0xffccdbe3);
+
 }
