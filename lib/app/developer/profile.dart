310a0fa7d465628b56d358767a9649f825febf6e
PHM-dung2 <pp4992@naver.com>
2025-05-01 18:21:33 +0900

    4/29 박희만 : 회원가입, 정보 수정 이미지 업로드 구현 완료 / 백, 프론트

--- lib/app/developer/profile.dart
+++ lib/app/developer/profile.dart
@@ -120,13 +120,7 @@ class _ProfileState extends State< Profile >{
 
     return CustomSingleChildScrollview(
       children: [
-        Text("기본 정보",
-          style: TextStyle(
-            fontFamily: "NanumGothic",
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        Text("기본 정보", style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold, ), ),
         SizedBox( height: 10 ,),
 
         // 첫번째 Card
@@ -234,21 +228,21 @@ class _ProfileState extends State< Profile >{
         ),
         SizedBox( height: 30,),
 
-        Text("비밀번호", style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold, ), ),
-        SizedBox( height: 15 ,),
 
         // 두번째 Card
+        Text("비밀번호", style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold, ), ),
+        SizedBox( height: 15 ,),
         CustomCard(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text("최근 업데이트 : ${developer['updateAt'].split('T')[0]}",
-                style: TextStyle( fontSize: 15, ),
+                style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey ),
               ),
               SizedBox( height: 5 ,),
 
-              Text("비밀번호", style: TextStyle( fontSize: 18, ), ),
-              SizedBox( height: 10 ,),
+              Text("비밀번호", style: TextStyle( fontSize: 16, fontWeight: FontWeight.bold ), ),
+              SizedBox( height: 15 ,),
 
               // 버튼
               Row(
@@ -269,8 +263,39 @@ class _ProfileState extends State< Profile >{
         SizedBox( height: 30,),
 
         // 세번째 Card
+        Text("계정 탈퇴", style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold, ), ),
+        SizedBox( height: 15 ,),
         CustomCard(
-          child : Text(""),
+          child : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("아이디 생성일 : ${developer['createAt'].split('T')[0]}",
+                style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey ),
+              ),
+              SizedBox( height: 5 ,),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB( 0, 0, 110, 0),
+                child: Text("계정 탈퇴시 프로필 및 프로젝트 관련 정보가 삭제됩니다.",
+                  style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold ), ),
+              ),
+              SizedBox( height: 15 ,),
+
+              // 버튼
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    CustomTextButton(
+                      onPressed: () => { setState(() => { isUpdate = false }) },
+                      title: "회원 탈퇴",
+                      width: 90,
+                      color: Colors.red,
+                    ),
+                  ]
+              ),
+            ],
+          ),
         ),
 
         SizedBox(height: 50 + MediaQuery.of(context).padding.bottom),
